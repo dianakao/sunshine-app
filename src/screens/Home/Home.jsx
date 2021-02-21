9355c97d2b56502a72624eb59f781de822adbe2c
Ken Ingram <ken.ingram@gmail.com>
2021-02-21 09:31:03 -0800

    Begin constructing landing page screen (Home.jsx)

---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Layout from '../../Components/shared/Layout/Layout'
+
+
+const Home = () => {
+
+  return (
+    <Layout />
+  )
+}
+
+export default Home
