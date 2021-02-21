d728401eb4a039e2f7602af0feace4f4414b2169
Ken Ingram <ken.ingram@gmail.com>
2021-02-21 11:29:21 -0800

    Renamed Components to components

---
+++ src/screens/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import Layout from '../../components/shared/Layout/Layout'
+
+
+const Home = () => {
+
+  return (
+    <Layout />
+  )
+}
+
+export default Home
