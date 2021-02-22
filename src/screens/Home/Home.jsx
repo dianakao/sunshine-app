a5731939f1bafaee153b8ea26cba293855d25a1a
Ian de Medeiros <44042532+iandemed@users.noreply.github.com>
2021-02-22 15:24:45 -0500

    Complete styling for home Screen

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -5,23 +5,31 @@ import LocationSearchBar from '../../components/LocationSearchBar/LocationSearch
 import GhostButton from '../../components/shared/GhostButton/GhostButton'
 
 
+import sunLogo from "../../assets/sun-logo.png"
+
 function Home({isLoaded}){
 
   return (
     <div className="home-container">
   
       <div className="hero-img">
-
+        <img src={sunLogo} alt=""/>
       </div>
 
       <div className="headline">
-        <h1>The Sunshine Collective</h1>
-        <h2> Access <span>solar energy</span> with ease</h2>
+        <h1 className="orange">The Sunshine Collective</h1>
+        <h2> Access <span className="orange">solar energy</span> with ease</h2>
       </div>
 
-      { isLoaded &&
-          <LocationSearchBar/>
-      }
+      <div className="search-wrapper">
+        { isLoaded &&
+            <LocationSearchBar/>
+        }
+        <div className="blurb-wrapper">
+          <h2 className> Get your Sunscore to see how solar power can help you!</h2>
+        </div>
+      </div>
+
       <div className="weather-wrapper">
         <h1>Current Conditions: Brooklyn</h1>
         <WeatherPanel/>
