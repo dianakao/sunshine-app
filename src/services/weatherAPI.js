9c86a99e5f54ef9476f7335f7aaf08fbf0700365
Ian de Medeiros <44042532+iandemed@users.noreply.github.com>
2021-02-22 17:41:07 -0500

    changed weather API request to https

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 let instance = axios.create({
-    baseURL: `http://api.openweathermap.org/data/2.5/weather?id=4254985&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=imperial`
+    baseURL: `https://api.openweathermap.org/data/2.5/weather?id=4254985&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=imperial`
 })
 
 export default instance
