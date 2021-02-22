9767c169c4c02f2312875964c5a98670a8d3b156
Ken Ingram <ken.ingram@gmail.com>
2021-02-21 19:09:46 -0800

    Only need 1 Results Screen component; Will adjust programmatically if necessary. The Overlay screen will hold the info toggles

---
+++ src/screens/Overlay/Overlay.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Overlay.css'
+import Layout from '../../components/shared/Layout/Layout'
+
+const Overlay = (props) => {
+  return (
+    <>
+      <div className="infobuttons">
+
+      </div>
+      <div className="infotext">
+
+      </div>
+    </>
+  )
+}
+
+export default Overlay
